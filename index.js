7ebf644b72697b88d20a32668e6a7515cbc2baa1
James Kim <jamesdaehokim@gmail.com>
2019-05-20 14:51:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,9 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toLowerCase)
+  console.log(string.toUpperCase)
+}
+
+function logWhisper(string) {
+  return console.log(string.toLowerCase())
 }
