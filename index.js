9a21a2f6330d63a4fbfc033eed67cca2c76081cf
James Kim <jamesdaehokim@gmail.com>
2019-05-20 15:13:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ function logWhisper(string) {
 
 
 
+
 function sayHiToGrandma(string) {
-  if (string = string.toLowerCase()) {
-  return "I can't hear you!"
+  if (string.toUpperCase()) {
+  return 'YES INDEED!'
 }
 }
